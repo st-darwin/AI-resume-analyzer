ef9a9bd2cda4a7240c87ea329c7fb9b267d61410
Darwin_10x <uzomasolomon289@gmail.com>
2026-03-10 19:14:33 +0100

    Update Summary component structure and styles
    Refactor Summary component layout and styling.

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -23,8 +23,8 @@ const Category = ({title , score } : {score : number , title : string}) =>{
 
 const Summary = ({feedback} : {feedback : Feedback}) => {
   return (
-    <div className='bg-white rounded-2xl shadow-md w-full '>
-      <div className='flex flex-row items-center p-4 gap-8 ' >
+    <div className='bg-white rounded-2xl  w-full '>
+      <div className='flex flex-row items-center p-2 gap-8 ' >
         <ScoreGauge score={feedback.overallScore}/>
         <div className='flex flex-col gap-2 ' >
         <h2 className='text-2xl font-semibold'>Your Resume Score</h2>
@@ -42,4 +42,3 @@ const Summary = ({feedback} : {feedback : Feedback}) => {
 }
 
 export default Summary
-
