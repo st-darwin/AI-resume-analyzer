7087b9421beea1581a5e71e966865965805869a6
Darwin_10x <uzomasolomon289@gmail.com>
2026-03-11 17:09:47 +0100

    Update Home.tsx

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -29,7 +29,7 @@ const navigate = useNavigate()
              
        
          {resumes.length > 0 && (
-            <div className='resumes-section'>
+            <div className='resumes-section grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 p-6"'>
                     {resumes.map((resume) =>(
                         <ResumeCard key={resume.id} resume={resume}/>
                     ))}
